813349c2c1bb752b5c0fd6fe413408751d158736
stan-dot <stanislawmalinowski09@gmail.com>
2022-11-16 22:43:56 +0000

    elaborated on the core idea

--- expo-todo-list/types/NF.ts
+++ expo-todo-list/types/NF.ts
@@ -29,87 +29,88 @@ const NF_CLASSICAL: Logic = {
 
 function construe_classical_nf_operators(): Operator[]{
   let operators: Operator[] = [];
+  return operators;
   //test[0]=("∈", "Inclusion", inclusion);
-  const nand: Operator = {
-    symbol: 'NAND',
-    name: 'Alternative denial',
-    action: (v1, v2) => 'true'
-  };
+  // const nand: Operator = {
+  //   symbol: 'NAND',
+  //   name: 'Alternative denial',
+  //   action: (v1, v2) => 'true'
+  // };
 
-  const altDenial = (first: boolean, second: boolean) => {
-    if (first === true && second === true) {
-      return false;
-    }
-    return true;
-  };
-  const membership = (first: any, second: any) => {
-  };
+  // const altDenial = (first: boolean, second: boolean) => {
+  //   if (first === true && second === true) {
+  //     return false;
+  //   }
+  //   return true;
+  // };
+  // const membership = (first: any, second: any) => {
+  // };
 
-  const universalQuantification = (array: any[], formula: (arg0: any) => any) => {
-    array.forEach((item: any, i: any) => {
-      if (!formula(item)) {
-        return false;
-      }
-    });
-    return true;
-  };
-
-
-
-  //D1
-  var denial = (first: any) => {
-    return altDenial(first, first);
-  };
-
-  test[0]=('NOT', 'Denial', denial);
-
-  //D2
-  var conjunction = (first: any, second: any) => {
-    return denial(altDenial(first, second));
-  };
-  test[0]=('AND', 'Conjuncion', conjunction);
-
-  //D3
-  var materialConditional = (first: any, second: any) => {
-    return altDenial(first, denial(second));
-  };
-  test[0]=('IMPLY', 'Material condition', materialConditional);
-
-  //D4
-  var alternation = (first: any, second: any) => {
-    return materialConditional(denial(first), second);
-  };
-  test[0]=('OR', 'Alternation', alternation);
-
-  //D5
-  var materialBiconditional = (first: any, second: any) => {
-    return altDenial(altDenial(first, second), alternation(first, second));
-  };
-  test[0]=('<=>', 'Material biconditional', materialBiconditional);
-
-  //D6
-  var existentialQuantification = (predicate: any, array: any) => {
-    //return (!forAll(array, predicate)); here quite problematic
-    return !universalQuantification(array, !predicate);
-  };
-  test[0]=('bigE', 'Existential quantification', existentialQuantification);
+  // const universalQuantification = (array: any[], formula: (arg0: any) => any) => {
+  //   array.forEach((item: any, i: any) => {
+  //     if (!formula(item)) {
+  //       return false;
+  //     }
+  //   });
+  //   return true;
+  // };
 
 
-  //D7
-  var supplementInclusionD7 = ((first: any, second: any) => {
-    return true;
-  });
 
-  //D11
-  var supplementAbstractionD11 = ((condition: (arg0: any) => any, array: any[]) => {
-    var result = [];
-    array.forEach((item: any, i: any) => {
-      if(condition(item){
-        result.push(item);
-      })
-    });
-    return result;
-  });
+  // //D1
+  // var denial = (first: any) => {
+  //   return altDenial(first, first);
+  // };
+
+  // test[0]=('NOT', 'Denial', denial);
+
+  // //D2
+  // var conjunction = (first: any, second: any) => {
+  //   return denial(altDenial(first, second));
+  // };
+  // test[0]=('AND', 'Conjuncion', conjunction);
+
+  // //D3
+  // var materialConditional = (first: any, second: any) => {
+  //   return altDenial(first, denial(second));
+  // };
+  // test[0]=('IMPLY', 'Material condition', materialConditional);
+
+  // //D4
+  // var alternation = (first: any, second: any) => {
+  //   return materialConditional(denial(first), second);
+  // };
+  // test[0]=('OR', 'Alternation', alternation);
+
+  // //D5
+  // var materialBiconditional = (first: any, second: any) => {
+  //   return altDenial(altDenial(first, second), alternation(first, second));
+  // };
+  // test[0]=('<=>', 'Material biconditional', materialBiconditional);
+
+  // //D6
+  // var existentialQuantification = (predicate: any, array: any) => {
+  //   //return (!forAll(array, predicate)); here quite problematic
+  //   return !universalQuantification(array, !predicate);
+  // };
+  // test[0]=('bigE', 'Existential quantification', existentialQuantification);
+
+
+  // //D7
+  // var supplementInclusionD7 = ((first: any, second: any) => {
+  //   return true;
+  // });
+
+  // //D11
+  // var supplementAbstractionD11 = ((condition: (arg0: any) => any, array: any[]) => {
+  //   var result = [];
+  //   array.forEach((item: any, i: any) => {
+  //     if(condition(item){
+  //       result.push(item);
+  //     })
+  //   });
+  //   return result;
+  // });
 
 
 }
